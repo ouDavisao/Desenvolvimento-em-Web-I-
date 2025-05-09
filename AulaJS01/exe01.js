a9f5d8206e5fe42f911e3ff16c325f5703a80b76
Davi Arthur Rodrigues de Magalhães <daviarthur8529@gmail.com>
2025-05-09 00:32:27 +0000

    aula de JavaScript 08/05/2025

---
+++ AulaJS01/exe01.js
@@ -0,0 +1,17 @@
+var nome = "Davi";
+console.log( typeof nome );
+
+nome = 19;
+console.log( typeof nome );
+
+let idade = 19;
+console.log( typeof idade );
+
+idade = "Davi";
+console.log( typeof idade );
+
+const cidade = "Patos de Minas";
+console.log( typeof cidade );
+
+cidade = "Uberlandia";
+console.log( typeof cidade );
